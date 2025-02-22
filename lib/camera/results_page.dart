5e1d609bcaeebf6c7bc9b6eb294cea86fe7b98aa
Sai Adithya Kothamasu <adithya261004@gmail.com>
2025-02-22 18:02:17 +0530

    dark mode added to camera results page

--- lib/camera/results_page.dart
+++ lib/camera/results_page.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:macrotracker/theme/app_theme.dart';
 
 class ResultsPage extends StatelessWidget {
   final String nutritionInfo;
@@ -12,30 +14,48 @@ class ResultsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Nutrition Results'),
-        backgroundColor: Colors.black,
-        foregroundColor: Colors.white,
+        leading: CupertinoNavigationBarBackButton(
+          color: Theme.of(context).primaryColor,
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        title: Text(
+          'Nutrition Results',
+          style: TextStyle(
+            color: Theme.of(context).primaryColor,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       ),
       body: SingleChildScrollView(
         padding: EdgeInsets.all(16.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          // crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Card(
               elevation: 4,
+              color:
+                  Theme.of(context).extension<CustomColors>()?.cardBackground,
               child: Padding(
                 padding: EdgeInsets.all(16.0),
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                  // crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(
-                      'Analysis Results',
-                      style: Theme.of(context).textTheme.headlineSmall,
-                    ),
-                    SizedBox(height: 16),
+                    // Text(
+                    //   'Analysis Results',
+                    //   style: TextStyle(
+                    //     color: Theme.of(context).primaryColor,
+                    //     fontWeight: FontWeight.w500,
+                    //     fontSize: 18,
+                    //   ),
+                    // ),
+                    // SizedBox(height: 16),
                     Text(
                       nutritionInfo,
-                      style: Theme.of(context).textTheme.bodyLarge,
+                      style: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 16),
                     ),
                   ],
                 ),
