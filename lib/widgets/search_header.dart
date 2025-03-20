fd2e16ffa33101d864f53d5aa81f93c646682338
Sai Adithya Kothamasu <adithya261004@gmail.com>
2025-03-20 15:09:05 +0530

    searchpage: some ui fixes

--- lib/widgets/search_header.dart
+++ lib/widgets/search_header.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:macrotracker/theme/app_theme.dart';
 import 'package:macrotracker/theme/typography.dart';
 import 'package:macrotracker/camera/camera.dart';
@@ -22,35 +24,17 @@ class SearchHeader extends StatefulWidget {
   State<SearchHeader> createState() => _SearchHeaderState();
 }
 
-class _SearchHeaderState extends State<SearchHeader>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _animationController;
-  late Animation<double> _buttonsAnimation;
+class _SearchHeaderState extends State<SearchHeader> {
   late FocusNode _focusNode;
 
   @override
   void initState() {
     super.initState();
-    _animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 200),
-    );
-    _buttonsAnimation = Tween<double>(begin: 1, end: 0).animate(
-      CurvedAnimation(parent: _animationController, curve: Curves.easeInOut),
-    );
     _focusNode = FocusNode();
-    _focusNode.addListener(() {
-      if (_focusNode.hasFocus) {
-        _animationController.forward();
-      } else {
-        _animationController.reverse();
-      }
-    });
   }
 
   @override
   void dispose() {
-    _animationController.dispose();
     _focusNode.dispose();
     super.dispose();
   }
@@ -111,6 +95,46 @@ class _SearchHeaderState extends State<SearchHeader>
                   ),
                 ),
               ),
+              // Camera button
+              Material(
+                color: Colors.transparent,
+                child: InkWell(
+                  borderRadius: BorderRadius.circular(40),
+                  onTap: () {
+                    HapticFeedback.lightImpact();
+                    _navigateToCameraScreen(context);
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.all(8),
+                    child: Icon(
+                      CupertinoIcons.camera,
+                      size: 25,
+                      color: const Color(0xFFFFC107),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(width: 8),
+              // AI button
+              Material(
+                color: Colors.transparent,
+                child: InkWell(
+                  borderRadius: BorderRadius.circular(40),
+                  onTap: () {
+                    HapticFeedback.lightImpact();
+                    _navigateToAskAI(context);
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.all(8),
+                    child: Image.asset(
+                      'assets/icons/AI Icon.png',
+                      width: 25,
+                      height: 25,
+                      color: const Color(0xFFFFC107),
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
           const SizedBox(height: 16),
@@ -126,107 +150,38 @@ class _SearchHeaderState extends State<SearchHeader>
                 ),
               ],
             ),
-            child: Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: widget.controller,
-                    focusNode: _focusNode,
-                    onChanged: widget.onChanged,
-                    onSubmitted: widget.onSearch,
-                    textInputAction: TextInputAction.search,
-                    style: AppTypography.body1.copyWith(
-                      color: customColors.textPrimary,
-                      fontWeight: FontWeight.w500,
-                    ),
-                    decoration: InputDecoration(
-                      hintText: 'Search foods, recipes, brands...',
-                      hintStyle: AppTypography.body1.copyWith(
-                        color: customColors.textPrimary.withOpacity(0.5),
-                      ),
-                      prefixIcon: Icon(
-                        Icons.search_rounded,
-                        color: customColors.textPrimary.withOpacity(0.7),
-                      ),
-                      suffixIcon: widget.controller.text.isNotEmpty
-                          ? IconButton(
-                              icon: Icon(Icons.close_rounded,
-                                  color: customColors.textPrimary
-                                      .withOpacity(0.7)),
-                              onPressed: () {
-                                widget.controller.clear();
-                                widget.onChanged('');
-                              },
-                            )
-                          : null,
-                      border: InputBorder.none,
-                      contentPadding: const EdgeInsets.symmetric(vertical: 16),
-                    ),
-                  ),
+            child: TextField(
+              controller: widget.controller,
+              focusNode: _focusNode,
+              onChanged: widget.onChanged,
+              onSubmitted: widget.onSearch,
+              textInputAction: TextInputAction.search,
+              style: AppTypography.body1.copyWith(
+                color: customColors.textPrimary,
+                fontWeight: FontWeight.w500,
+              ),
+              decoration: InputDecoration(
+                hintText: 'Search foods, recipes, brands...',
+                hintStyle: AppTypography.body1.copyWith(
+                  color: customColors.textPrimary.withOpacity(0.5),
                 ),
-                SizeTransition(
-                  sizeFactor: _buttonsAnimation,
-                  axis: Axis.horizontal,
-                  child: Row(
-                    children: [
-                      Material(
-                        color: Colors.transparent,
-                        child: InkWell(
-                          borderRadius: BorderRadius.circular(0),
-                          onTap: () => _navigateToCameraScreen(context),
-                          child: Container(
-                            height: 56,
-                            width: 48,
-                            decoration: BoxDecoration(
-                              color: Colors.white.withOpacity(0.1),
-                              border: Border.all(
-                                color: Colors.grey.withOpacity(0.2),
-                                width: 0.5,
-                              ),
-                            ),
-                            child: Center(
-                              child: Icon(
-                                Icons.camera_alt_rounded,
-                                color: const Color(0xFFFFC107),
-                                size: 22,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      Material(
-                        color: Colors.transparent,
-                        child: InkWell(
-                          borderRadius: const BorderRadius.horizontal(
-                              right: Radius.circular(16)),
-                          onTap: () => _navigateToAskAI(context),
-                          child: Container(
-                            height: 56,
-                            width: 48,
-                            decoration: BoxDecoration(
-                              color: Colors.white.withOpacity(0.1),
-                              borderRadius: const BorderRadius.horizontal(
-                                  right: Radius.circular(16)),
-                              border: Border.all(
-                                color: Colors.grey.withOpacity(0.2),
-                                width: 0.5,
-                              ),
-                            ),
-                            child: Center(
-                              child: Image.asset(
-                                'assets/icons/AI Icon.png',
-                                width: 22,
-                                height: 22,
-                                color: const Color(0xFFFFC107),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+                prefixIcon: Icon(
+                  Icons.search_rounded,
+                  color: customColors.textPrimary.withOpacity(0.7),
                 ),
-              ],
+                suffixIcon: widget.controller.text.isNotEmpty
+                    ? IconButton(
+                        icon: Icon(Icons.close_rounded,
+                            color: customColors.textPrimary.withOpacity(0.7)),
+                        onPressed: () {
+                          widget.controller.clear();
+                          widget.onChanged('');
+                        },
+                      )
+                    : null,
+                border: InputBorder.none,
+                contentPadding: const EdgeInsets.symmetric(vertical: 16),
+              ),
             ),
           ),
         ],
